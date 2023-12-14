4018dffecae79e2a36035c2c08a3962104f6c63d
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2023-12-14 18:03:56 +0300

    Time: 103 ms (45.2%), Space: 20.5 MB (39.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,11 @@
+from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         def anagrams(strs):
-            anagram = {}
+            anagram = defaultdict(list)
             for i in range(len(strs)):
-                if tuple(sorted(strs[i])) in anagram.keys():
-                    anagram[tuple(sorted(strs[i]))].append(strs[i])
-                else:
-                    anagram[tuple(sorted(strs[i]))] = [strs[i]] 
-                
+                sort_s = tuple(sorted(strs[i]))
+                anagram[sort_s].append(strs[i])
             return anagram.values()
         return anagrams(strs)
         
