7cbdf11813d7b909ce0469d619fb899e00c9db1d
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-01-24 23:57:37 +0300

    Time: 203 ms (66.68%), Space: 30.4 MB (55.22%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,13 +5,13 @@ class Solution:
         # nums = sorted(nums)
         temp = 0
         while r<len(nums):
-            temp += nums[r]
 
-            while temp>= target:
+            while temp + nums[r]>= target:
                 result = min(result,r-l+1)
                 temp -= nums[l]
                 l+=1
-                
+            
+            temp += nums[r]
             r+=1
         if result != float('inf'):
             return result
