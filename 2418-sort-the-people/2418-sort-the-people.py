2216a6bcaaca4ccac5539711fa23c1ea91192a3f
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-01-28 09:22:54 +0300

    Time: 103 ms (83.74%), Space: 17.1 MB (64.03%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,11 +1,7 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        def people(names,heights):
-            hashe = dict(zip(heights,names))
-            heights.sort(reverse=True)
-            ans = []
-            for i in heights:
-                ans.append(hashe[i])
-            return ans
-        return people(names,heights)
-        
+        combined = zip(names,heights)
+        combined = sorted(combined,key = lambda x: x[1],reverse =True )
+        result = []
+        for i,j in combined: result.append(i)
+        return result
