7d7f2e5d7836b554e5767509ec2b98de1b733a85
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-02-14 14:56:26 +0300

    Add solution for Jump Game

---
+++ leetcode_solutions/leetcode/jump-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) == 1: return True
+        count = 0
+        good = nums[-1]
+        for i in range(len(nums)-1,0,-1):
+            if nums[i-1] > count:
+                good = nums[i-1]
+                count = 0
+                if i == 1: return True
+            else: count += 1
+        return False
