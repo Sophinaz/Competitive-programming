df710dc3b3221dea53ba2834c861bd245da88b56
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-02-14 21:31:25 +0300

    Add solution for Valid Palindrome

---
+++ leetcode_solutions/leetcode/valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s1 = ''
+        s = s.lower()
+        for c in s:
+            if c.isalnum():
+                s1 += c
+
+        return True if s1==s1[::-1] else False
