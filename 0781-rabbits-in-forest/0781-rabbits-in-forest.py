85715b3f2c6c84b57f7fb6754ac58696eb5929f4
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-02-15 10:34:05 +0300

    Time: 46 ms (48.36%), Space: 16.7 MB (71.27%) - LeetHub

---
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        answers.sort()
+        track = {}
+        result = 0
+
+        for i in answers:
+            if i not in track and i != 0:
+                track[i] = i
+                result += i+1
+            elif i == 0:
+                result += 1
+            else: 
+                track[i] -= 1
+                if track[i] == 0: track.pop(i)
+
+        return result
+
+            
+
