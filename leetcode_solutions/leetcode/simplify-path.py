c87a913ee15080d5032f1d8fd4e0b30e1402f097
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-02-17 22:25:26 +0300

    Add solution for Simplify Path

---
+++ leetcode_solutions/leetcode/simplify-path.py
@@ -0,0 +1,13 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        path = path.split("/")
+        for i in path:
+            if i == "" or i == '.':
+                continue
+            elif i == '..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(i)
+        return '/' + "/".join(stack)
