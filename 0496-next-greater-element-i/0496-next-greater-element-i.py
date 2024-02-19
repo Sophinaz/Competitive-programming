602e101536f7fd36d9c352dcdde79cdc1a55871c
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-02-19 10:36:52 +0300

    Time: 39 ms (95.67%), Space: 16.8 MB (93.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,9 +1,21 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-    
+        track = {}
+        stack = []
+        answer = [-1] * len(nums1)
 
-        
+        for i in nums2:
+            if not stack: stack.append(i)
 
+            if i > stack[-1]: 
+                while stack and i > stack[-1]:
+                    x = stack.pop()
+                    track[x] = i
+                stack.append(i)
+            
+            if stack and i < stack[-1]:
+                stack.append(i)
 
-        
-        
+        for i in range(len(nums1)):
+            answer[i] = track.get(nums1[i],-1)
+        return answer
