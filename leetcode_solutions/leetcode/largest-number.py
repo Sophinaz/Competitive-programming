13a97c15540975f83b0feea65169051cb5b7528b
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-02-20 21:46:27 +0300

    Add solution for Largest Number

---
+++ leetcode_solutions/leetcode/largest-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def largestNumber(self, nums):
+        if (sum(nums)) == 0:
+            return "0"
+        def compare(x,y):
+            return x+y > y+x
+        nums = [str(i) for i in nums]
+        nums.sort(reverse=True)
+        j = 1
+        for i in range(len(nums)-1):
+            j = i+1
+            while j < len(nums):
+                if not compare(nums[i],nums[j]):
+                    nums[i],nums[j] = nums[j],nums[i]
+                j += 1
+        return "".join(nums)
