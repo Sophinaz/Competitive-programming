660de628ed26f8901f5aa67e86d9b337e432921e
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-04-12 17:31:22 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxDistance(self, grid: List[List[int]]) -> int:
+        visited = [[False for _ in range(len(grid[0]))] for j in range(len(grid))]
+        direction = [[0,1], [1,0], [-1,0], [0,-1]]
+        
+        def valid(row, col):
+            return 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] != 1
+        que = deque()
+        later = []
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    que.append((i,j))
+                    later.append((i,j))
+                    visited[i][j] = True
+        if not later: return -1
+        level = 0
+        while que:
+            for _ in range(len(que)):
+                row, col = que.popleft()
+                for r, c in direction:
+                    new_row = row + r
+                    new_col = col + c
+                    if valid(new_row, new_col) and not visited[new_row][new_col]:
+                        que.append((new_row, new_col))
+                        visited[new_row][new_col] = True
+            level += 1
+        result = float('inf')
+        for r, c in later:
+            result = min(result, abs(row-r) + abs(col-c))
+        return result
