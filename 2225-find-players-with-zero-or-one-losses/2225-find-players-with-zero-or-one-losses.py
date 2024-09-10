8af36584711dfbe83c1981242e4df94e9596d248
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-09-10 14:46:36 +0300

    Time: 1437 ms (28.86%), Space: 71.2 MB (75.76%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,22 +1,19 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        track0 = {}
-        track1 = {}
-        ans1 = []
-        ans2 = []
-        for i,j in matches:
-            if j not in track1:
-                if j not in track0: track1[j] = 1
-            else: 
-                track1.pop(j)
-                track0[j] = 1
+        income = {}
+        for a, b in matches:
+            if b not in income:
+                income[b] = 1
+            else: income[b] += 1
 
-
-        for i,j in matches:
-            if i not in  track1 and i not in track0 and i not in ans1:
-                ans1.append(i)
-        ans2 = list(set(track1.keys()))
-        ans1.sort()
-        ans2 = sorted(ans2)
-
-        return [ans1,ans2]
+            if a not in income:
+                income[a] = 0
+        result = [[] for i in range(2)]
+        for i in income:
+            if income[i] == 0:
+                result[0].append(i)
+            elif income[i] == 1:
+                result[1].append(i)
+        result[0].sort()
+        result[1].sort()
+        return result
