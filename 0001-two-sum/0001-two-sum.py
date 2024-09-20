3edc7be5ccb5ce6e59b69467e0c11f1a88a0cdca
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2024-09-20 20:03:43 +0300

    Time: 32 ms (93.12%), Space: 12.6 MB (44.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,6 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+
         def sumOfElements(nums,target):
             has = {}
             for i in range(len(nums)):
