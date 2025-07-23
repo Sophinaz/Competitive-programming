df5cd01a405608d0b01f9b84f3a47b49b2796b43
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2025-07-23 15:06:46 +0300

    Time: 202 ms (87.06%), Space: 17.2 MB (100%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.py
+++ 0835-image-overlap/0835-image-overlap.py
@@ -1,18 +1,20 @@
 class Solution:
     def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
-        overlap = defaultdict(int)
-
-        def calculate_overlap(row, col):
-            for row_2 in range(len(img2)):
-                for col_2 in range(len(img2[0])):
-                    if img2[row_2][col_2]:
-                        difference = (row - row_2, col - col_2)
-                        overlap[difference] += 1
-
-
-        for row in range(len(img1)):
-            for col in range(len(img1[0])):
-                if img1[row][col]:
-                    calculate_overlap(row, col)
-
-        return max(list(overlap.values())) if len(overlap) else 0
+        ones = []
+        track = defaultdict(int)
+        for i in range(len(img1)):
+            for j in range(len(img1[0])):
+                if img1[i][j] == 1: 
+                    ones.append([i,j])
+        for i in range(len(img2)):
+            for j in range(len(img2[0])):
+                if img2[i][j] == 1:
+                    for a, b in ones:
+                        diff = (i - a, j - b)
+                        track[diff] += 1
+        maxx = 0
+        for i in track:
+            if track[i] > maxx:
+                maxx = track[i]
+        return maxx
+        
