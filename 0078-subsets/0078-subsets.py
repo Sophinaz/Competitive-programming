ffbfd347efba3520a1b6756340e9106c3e829fce
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2025-11-10 16:57:16 +0300

    Time: 0 ms (100%), Space: 17.6 MB (99.86%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,16 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        maxx = 2 ** len(nums)
         result = []
 
-        for number in range(maxx):
-            temp_res = []
-            idx = len(nums) - 1
-            while number:
-                if number & 1:
-                    temp_res.append(nums[idx])
-                idx -= 1
-                number >>= 1
-            result.append(temp_res)
+        def backtrack(i, path):
+            if i == len(nums):
+                result.append(path[:])
+                return 
 
+            path.append(nums[i])
+            backtrack(i+1, path) 
+            path.pop()
+            backtrack(i+1, path)
+
+        backtrack(0, [])
         return result
+            
