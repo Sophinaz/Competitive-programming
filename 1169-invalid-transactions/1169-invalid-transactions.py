f8943e3a7e960113a46f737434182d3b9eef9a30
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2025-11-12 15:04:14 +0300

    Time: 14 ms (67.49%), Space: 18.9 MB (8.61%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,34 +1,27 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        invalid = []
         track = defaultdict(lambda: defaultdict(set))
-        invalids = []
-
-        for i in range(len(transactions)):
-            # split into four 
-            name, time , amount, place = transactions[i].split(',')
-            # add them to hashmap
-            track[int(time)][name].add(place)
 
         for transaction in transactions:
-            isinvalid = False
-            # split into four
-            name, time, amount, place = transaction.split(',')
-            # check if amount excedes 1000 
-            if int(amount) > 1000:
-                isinvalid = True
+            name, time, amount, city = transaction.split(",")
+            time = int(time)
+            amount = int(amount)
+            track[time][name].add(city)
 
-            # check for invalidity by place
-            for ti in range(-60, 61):
-                curr_time = int(time) + ti
-                # if curr_time already exisits
-                if curr_time in track:
-                    if name in track[curr_time]:
-                        if len(track[curr_time][name]) > 1 or (place not in track[curr_time][name]):
-                            isinvalid = True
+        for transaction in transactions:
+            name, time, amount, city = transaction.split(",")
+            time = int(time)
+            amount = int(amount)
 
-            # if invalid add to result
-            if isinvalid:
-                invalids.append(transaction)
+            if amount > 1000:
+                invalid.append(transaction)
+                continue
 
-        return invalids
+            for timerange in range(time-60, time+61):
+                if timerange in track and name in track[timerange]:
+                    if len(track[timerange][name]) > 1 or city not in track[timerange][name]:
+                        invalid.append(transaction)
+                        break
 
+        return invalid
