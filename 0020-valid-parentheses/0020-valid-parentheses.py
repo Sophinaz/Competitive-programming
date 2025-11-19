88f8c38e45a702c36b748b3e25abb8589673fe84
Sophonias Demiss <116633530+Sophinaz@users.noreply.github.com>
2025-11-19 11:01:54 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,14 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        tracker = {")": "(", "}": "{", "]": "["}
         stack = []
-        for i in s:
-            if stack and i in tracker:
-                if stack[-1] == tracker[i]:
-                    stack.pop()
-                else:
-                    return False
+        check = {")": "(", "]": "[", "}": "{"}
+
+        for char in s:
+            if char in check:
+                if stack and stack.pop() == check[char]:
+                    continue
+                return False
             else:
-                stack.append(i)
-        if stack: return False
+                stack.append(char)
+
         return True
